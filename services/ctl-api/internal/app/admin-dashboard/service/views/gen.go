691a2761070af3c0d4174ac7013dd979b9fe443f
Nat Hamilton <info@nathamilton.com>
2026-02-19 10:18:05 -0500

    chore(admin-ui): added --path to go gen templ generate cmd (#414)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>
    Co-authored-by: nuonbot <bot@nuon.co>

--- services/ctl-api/internal/app/admin-dashboard/service/views/gen.go
+++ services/ctl-api/internal/app/admin-dashboard/service/views/gen.go
@@ -1,3 +1,3 @@
 package views
 
-//go:generate go run github.com/a-h/templ/cmd/templ@latest generate
+//go:generate go run github.com/a-h/templ/cmd/templ@latest generate --path=./internal/app/admin-dashboard
