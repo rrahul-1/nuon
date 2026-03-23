337feea368154ee0800a6496dd9c429a7e9cd492
Nat Hamilton <info@nathamilton.com>
2026-03-23 14:50:38 -0400

    refactor(api): don't return approvals for steps that have auto-skipped (#663)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/ctl-api/internal/pkg/db/viewsql/workflow_step_approvals_pending_view_v1.sql
+++ services/ctl-api/internal/pkg/db/viewsql/workflow_step_approvals_pending_view_v1.sql
@@ -3,5 +3,9 @@ FROM install_workflow_step_approvals a
 LEFT JOIN install_workflow_step_approval_responses resp
   ON resp.install_workflow_step_approval_id = a.id
   AND resp.deleted_at = 0
+JOIN install_workflow_steps s
+  ON s.id = a.install_workflow_step_id
+  AND s.deleted_at = 0
 WHERE a.deleted_at = 0
   AND resp.id IS NULL
+  AND (s.status->>'status') != 'auto-skipped'
