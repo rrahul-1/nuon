c41e13f6c49ff19bf8a2f21aacbe19b7aa8825ad
Nat Hamilton <info@nathamilton.com>
2026-05-08 12:42:44 -0400

    fix(ui): log panel copy link (#1244)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/client/hooks/use-full-url.ts
+++ services/dashboard-ui/client/hooks/use-full-url.ts
@@ -1,22 +1,6 @@
-import { useLocation, useSearchParams } from 'react-router'
-import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router'
 
 export function useFullUrl() {
-  const { pathname } = useLocation()
-  const [searchParams] = useSearchParams()
-  const [fullUrl, setFullUrl] = useState<string>('')
-
-  useEffect(() => {
-    const url = new URL(pathname, window.location.origin)
-
-    if (searchParams) {
-      searchParams.forEach((value, key) => {
-        url.searchParams.set(key, value)
-      })
-    }
-
-    setFullUrl(url.toString())
-  }, [pathname, searchParams])
-
-  return fullUrl
+  const { pathname, search } = useLocation()
+  return `${window.location.origin}${pathname}${search}`
 }
