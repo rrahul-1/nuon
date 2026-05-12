7867ea88097fcd0c9de755026cfc16f6993dcc9d
Somesh Koli <somesh@nuon.co>
2026-05-12 19:19:11 +0530

    fix: capture refs only till empty space to avoid capturing extra templating information (#1271)
    fix: capture refs only till empty space to avoid capturing extra
    templating information

--- pkg/config/refs/refs.go
+++ pkg/config/refs/refs.go
@@ -76,13 +76,13 @@ func Parse(obj any) ([]Ref, error) {
 // https://pkg.go.dev/text/template/parse
 func ParseFieldRefs(inputVar string) []Ref {
 	refPatterns := map[RefType]string{
-		RefTypeComponents:    `nuon\.components\.([^.}]+)\.outputs\.([^}]+)`,
-		RefTypeActions:       `nuon\.actions\.([^.}]+)\.outputs\.([^.}]+)`,
-		RefTypeSecrets:       `nuon\.secrets\.([^.}]+)`,
-		RefTypeInputs:        `nuon\.inputs\.inputs\.([^.}]+)`,
-		RefTypeInstallInputs: `nuon\.install\.inputs\.([^.}]+)`,
-		RefTypeInstallStack:  `nuon\.install_stack\.outputs\.([^.}]+)`,
-		RefTypeSandbox:       `nuon\.sandbox\.outputs\.([^.}]+)`,
+		RefTypeComponents:    `nuon\.components\.([^.}]+)\.outputs\.([^}\s]+)`,
+		RefTypeActions:       `nuon\.actions\.([^.}]+)\.outputs\.([^.}\s]+)`,
+		RefTypeSecrets:       `nuon\.secrets\.([^.}\s]+)`,
+		RefTypeInputs:        `nuon\.inputs\.inputs\.([^.}\s]+)`,
+		RefTypeInstallInputs: `nuon\.install\.inputs\.([^.}\s]+)`,
+		RefTypeInstallStack:  `nuon\.install_stack\.outputs\.([^.}\s]+)`,
+		RefTypeSandbox:       `nuon\.sandbox\.outputs\.([^.}\s]+)`,
 	}
 
 	refs := make([]Ref, 0)
