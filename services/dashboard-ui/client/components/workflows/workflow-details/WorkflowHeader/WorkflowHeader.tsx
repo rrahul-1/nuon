296ef9cf0716de5bc65f475f5aca25a4d161d22e
Nat Hamilton <info@nathamilton.com>
2026-05-18 16:06:00 -0400

    fix(ui): all workflows goes to workflows page (#1367)
    Signed-off-by: Nat Hamilton <info@nathamilton.com>

--- services/dashboard-ui/client/components/workflows/workflow-details/WorkflowHeader/WorkflowHeader.tsx
+++ services/dashboard-ui/client/components/workflows/workflow-details/WorkflowHeader/WorkflowHeader.tsx
@@ -1,4 +1,4 @@
-import { BackLink } from '@/components/common/BackLink'
+import { Link, useParams } from 'react-router'
 import { Badge } from '@/components/common/Badge'
 import { HeadingGroup } from '@/components/common/HeadingGroup'
 import { Icon } from '@/components/common/Icon'
@@ -13,6 +13,7 @@ interface IWorkflowHeader {
 }
 
 export const WorkflowHeader = ({ workflow, install }: IWorkflowHeader) => {
+  const { orgId, installId } = useParams()
   const hasDrift =
     install?.drifted_objects?.length &&
     install?.drifted_objects?.find(
@@ -22,9 +23,12 @@ export const WorkflowHeader = ({ workflow, install }: IWorkflowHeader) => {
   return (
     <div className="flex flex-wrap items-center gap-3 justify-between w-full">
       <div className="flex flex-col gap-4">
-        <BackLink>
+        <Link
+          to={`/${orgId}/installs/${installId}/workflows`}
+          className="flex items-center gap-1.5 w-fit text-base leading-6 tracking-[-0.2px] font-strong text-primary-600 dark:text-primary-500 hover:text-primary-800 hover:dark:text-primary-400 focus:text-primary-800 focus:dark:text-primary-400 active:text-primary-900 active:dark:text-primary-600 focus-visible:rounded"
+        >
           <Icon variant="CaretLeftIcon" weight="bold" /> All workflows
-        </BackLink>
+        </Link>
         <HeadingGroup>
           <Text
             flex
